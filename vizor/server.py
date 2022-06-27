d74d5b569465b5b764b2d526c15282fbbf0fdc40
AcidBrother <52380857+AcidBrother@users.noreply.github.com>
2022-06-27 20:57:28 +0300

    1

--- vizor/server.py
+++ vizor/server.py
@@ -49,16 +49,40 @@ def db_users_add(name, login):
     slog = "Added " + name + " " + login + "to users"
     db_call(s, slog)
 
+def db_users_select_by_id(rowid):
+    s = '''SELECT * FROM users WHERE ROWID =''' +  rowid
+    slog = "Selected " + rowid + " user"
+    return db_call_select(s,slog)
+
+def db_users_select_all():
+    s = '''SELECT * FROM users '''
+    slog = "Selected all " + " user"
+    return db_call_select(s,slog)
+
 def db_helpers_create():
     s = '''CREATE TABLE IF NOT EXISTS helpers (name TEXT NOT NULL, login TEXT NOT NULL, user_id INTEGER NOT NULL)'''
     slog = "created helpers table"
-    db_call(s, slog)
+    db_call_select(s, slog)
 
 def db_helpers_add(name, login, user_id):
     s = '''INSERT OR IGNORE INTO helpers (name, login, user_id) VALUES ("'''  + name +'''","''' + login + '''",''' + user_id + ''')'''
     slog = "Added " + name + " " + login + " " + user_id + "to helpers"
     db_call(s, slog)
 
+def db_helpers_select_by_id(rowid):
+    s = '''SELECT * FROM helpers WHERE ROWID =''' +  rowid
+    slog = "Selected " + rowid + " helper"
+    return db_call_select(s,slog)
+
+def db_helpers_select_by_user_id(user_id):
+    s = '''SELECT * FROM helpers WHERE user_id =''' +  user_id
+    slog = "Selected by" + user_id + " helper"
+    return db_call_select(s,slog)
+
+def db_helpers_select_all():
+    s = '''SELECT * FROM helpers '''
+    slog = "Selected all " + " helper"
+    return db_call_select(s,slog)
 
 def db_call(s,slog):
     con = sqlite3.connect('test.db')
@@ -67,6 +91,19 @@ def db_call(s,slog):
     con.commit()
     con.close()
     print(slog)
+
+def db_call_select(s,slog):
+    con = sqlite3.connect('test.db')
+    cur = con.cursor()
+    cur.execute(s)
+    rows = cur.fetchall()
+    con.close()
+    print(slog)
+    return rows
+
+
+#=========================================================
+#=========================================================
 
 
 @app.route("/")
@@ -82,10 +119,10 @@ def hanlder_db_users_create():
     db_users_create()
     return "<p>Created users table</p>"
 
-@app.route('/db/users/insert', methods = ['GET'])
+@app.route('/db/users/insert', methods = ['GET','POST'])
 def hanlder_db_users_insert():
     req_is_ok = False
-    if request.method == 'GET':
+    if (request.method == 'POST'):
         arg_name  = request.args.get('name')
         arg_login = request.args.get('login')
         if not((arg_name is None) or (arg_login is None)):
@@ -93,11 +130,94 @@ def hanlder_db_users_insert():
     if req_is_ok:
         db_users_add(arg_name, arg_login)
         resp = jsonify(success=True)
-        return "<p>Added new user</p>"
+        return resp
     else:
-        return "<p>Error, not enough args</p>"
+        return "Incorrect request", 400
+
+@app.route('/db/users/get', methods = ['GET','POST'])
+def hanlder_db_users_get_by_id():
+    req_is_ok = False
+    if (request.method == 'GET'):
+        arg_id  = request.args.get('id')
+        if not(arg_id is None):
+            req_is_ok = True
+        if req_is_ok:
+            res = db_users_select_by_id(arg_id)
+            print(res)
+            return jsonify(res)
+        else:
+            return "Incorrect request", 400
+
+@app.route('/db/users/getall', methods = ['GET','POST'])
+def hanlder_db_users_get_all():
+    req_is_ok = False
+    if (request.method == 'GET'):
+        req_is_ok = True
+    if req_is_ok:
+        res = db_users_select_all()
+        print(res)
+        return jsonify(res)
+    else:
+        return "Incorrect request", 400
 
 @app.route('/db/helpers/create')
 def hanlder_db_helpers_create():
-    db_heplers_create()
-    return "<p>Created helpers table</p>"
+    db_helpers_create()
+    return "<p>Created helpers table</p>", 200
+
+@app.route('/db/helpers/insert', methods = ['GET','POST'])
+def hanlder_db_helpers_insert():
+    req_is_ok = False
+    if (request.method == 'POST'):
+        arg_name  = request.args.get('name')
+        arg_login = request.args.get('login')
+        arg_user_id = request.args.get('user_id')
+        if not((arg_name is None) or (arg_login is None) or (arg_user_id is None)):
+            req_is_ok = True
+    if req_is_ok:
+        db_helpers_add(arg_name, arg_login, arg_user_id)
+        resp = jsonify(success=True)
+        return resp
+    else:
+        return "Incorrect request", 400
+
+
+@app.route('/db/helpers/get', methods = ['GET','POST'])
+def hanlder_db_helpers_get_by_id():
+    req_is_ok = False
+    if (request.method == 'GET'):
+        arg_id  = request.args.get('id')
+        if not(arg_id is None):
+            req_is_ok = True
+        if req_is_ok:
+            res = db_helpers_select_by_id(arg_id)
+            print(res)
+            return jsonify(res)
+        else:
+            return "Incorrect request", 400
+
+@app.route('/db/helpers/getbyuser', methods = ['GET','POST'])
+def hanlder_db_helpers_get_by_user_id():
+    req_is_ok = False
+    if (request.method == 'GET'):
+        arg_user_id  = request.args.get('user_id')
+        if not(arg_user_id is None):
+            req_is_ok = True
+        if req_is_ok:
+            res = db_helpers_select_by_user_id(arg_user_id)
+            print(res)
+            return jsonify(res)
+        else:
+            return "Incorrect request", 400
+
+@app.route('/db/helpers/getall', methods = ['GET','POST'])
+def hanlder_db_helpers_get_all():
+    req_is_ok = False
+    if (request.method == 'GET'):
+        req_is_ok = True
+    if req_is_ok:
+        res = db_helpers_select_all()
+        print(res)
+        return jsonify(res)
+    else:
+        return "Incorrect request", 400
